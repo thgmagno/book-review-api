2e089943de4689c6ee6d2959a89765e65fee5603
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-08-20 10:04:14 -0300

    add relation reviews > books

--- database/migrations/2024_08_20_130115_create_reviews_table.php
+++ database/migrations/2024_08_20_130115_create_reviews_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,13 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+
             $table->text('review');
             $table->unsignedTinyInteger('rating');
+
             $table->timestamps();
+
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
         });
     }
 
